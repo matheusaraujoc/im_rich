dd95740e77954a9618964eb6f712046d1ee3657b
matheusaraujoc <theus461001@gmail.com>
2024-09-14 11:28:30 -0300

    features: Appbar and Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Olá, mundo!'),
+      home: Scaffold(
+        backgroundColor: Colors.grey[700],
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blue[200],
+          title: Text('Eu sou rico'),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://i.pinimg.com/736x/31/65/f3/3165f3d062c8d43cfdd0c836e1811075.jpg'),
+          ),
+        ),
       ),
     ),
   );
